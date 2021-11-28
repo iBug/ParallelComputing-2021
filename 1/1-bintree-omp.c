0e52c0fe3283c2c4fbe661a2f17c3cd602cc067f
iBug <git@ibugone.com>
2021-11-29 00:54:37 +0800

    VSCode Shift-Alt-F

--- 1/1-bintree-omp.c
+++ 1/1-bintree-omp.c
@@ -1,13 +1,13 @@
+#include <omp.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <omp.h>
 
 int popcnt(uint32_t i) {
-     i = i - ((i >> 1) & 0x55555555);
-     i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
-     i = (i + (i >> 4)) & 0x0F0F0F0F;
-     return (i * 0x01010101) >> 24;
+    i = i - ((i >> 1) & 0x55555555);
+    i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+    i = (i + (i >> 4)) & 0x0F0F0F0F;
+    return (i * 0x01010101) >> 24;
 }
 
 int main() {
@@ -24,7 +24,7 @@ int main() {
 
     // Reduce sum
     for (int i = 1; i < n; i <<= 1) {
-        #pragma omp parallel for
+#pragma omp parallel for
         for (int j = 0; j < n; j += i << 1) {
             A[j] += A[j + i];
         }
@@ -32,7 +32,7 @@ int main() {
 
     // Broadcast
     for (int i = n << 1; i > 0; i >>= 1) {
-        #pragma omp parallel for
+#pragma omp parallel for
         for (int j = 0; j < n; j += i << 1) {
             A[j + i] = A[j];
         }
